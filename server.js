f0d7b95fc9709518c4de667c3d4477c1d5d3de96
Blake William Pierce <Darkfire1026@gmail.com>
2019-12-14 23:22:19 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,149 @@
+var mysql = require("mysql");
+var express = require("express");
+var inquirer = require("inquirer");
+var consoleTable = require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Blake1026",
+    database: "work_db"
+});
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    start();
+});
+
+
+function start() {
+    inquirer
+        .prompt([{
+            type: "list",
+            name: "selection",
+            message: "What do you want to do?",
+            choices: [
+                "Add a department",
+                "Add a job",
+                "Add a worker",
+                "View a worker",
+                "Update a worker",
+                "View a department",
+                "View a job"
+            ]
+        }])
+
+    .then(function(response) {
+        console.log(response);
+        switch (response.selection) {
+            case "Add a department":
+                getDepartmentInfo();
+                break;
+            case "Add a job":
+                getjobInfo();
+                break;
+            case "Add a worker":
+                getworkerInfo();
+                break;
+            case "View a department":
+                viewDepartmentInfo();
+                break;
+            case "View a job":
+                viewjobInfo();
+                break;
+            case "View a worker":
+                viewworkerInfo();
+                break;
+            case "Update a worker":
+                updateDepartmentInfo();
+                break;
+            default:
+                break;
+        }
+    });
+}
+
+function getDepartmentInfo() {
+    console.log(name + "input");
+    inquirer
+        .prompt([{
+            type: "input",
+            name: "name",
+            message: "What is the department name?"
+        }])
+        .then(function(response) {
+            console.log(response);
+        });
+}
+
+function getjobInfo() {
+    console.log(name + "input");
+    inquirer
+        .prompt([{
+                type: "input",
+                name: "name",
+                message: "What is the job's title?"
+            },
+            {
+                type: "input",
+                name: "name",
+                message: "What is the salary for this job?"
+            },
+            {
+                type: "input",
+                name: "name",
+                message: "What is this job's department id?"
+            }
+        ])
+        .then(function(response) {
+            console.log(response);
+        });
+}
+
+function getworkerInfo() {
+    console.log("");
+    inquirer
+        .prompt([{
+                type: "input",
+                name: "name",
+                message: "What is the worker's first name?"
+            },
+            {
+                type: "input",
+                name: "name",
+                message: "What is the worker's last name?"
+            },
+            {
+                type: "input",
+                name: "name",
+                message: "What is the worker's job id?"
+            },
+            {
+                type: "input",
+                name: "name",
+                message: "What is the id of the worker's manager?"
+            }
+        ])
+        .then(function(response) {
+            console.log(response);
+        });
+}
+
+function viewDepartmentInfo() {
+    console.log("getDepartmentInfo");
+}
+
+function viewjobInfo() {
+    console.log("getjobInfo");
+}
+
+function viewworkerInfo() {
+    console.log("getworkerInfo");
+}
+
+function updateworkerInfo() {
+    console.log("getworkerInfo");
+}
+
+connection.end();
